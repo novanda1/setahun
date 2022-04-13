d81e86ce43538d2c203bc3d6c3e88e4e5ffb840c
Novanda Ahsan <novandaahsan1@gmail.com>
2022-04-14 00:08:12 +0700

    better auth

--- pages/login.tsx
+++ pages/login.tsx
@@ -17,7 +17,7 @@ function LoginPage() {
   return (
     <>
       <div className='flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900'>
-        <div className='flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800 h-96'>
+        <div className='flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800'>
           <div className='flex flex-col overflow-y-auto md:flex-row'>
             <div className='relative h-32 md:h-auto md:w-1/2'>
               <Image
