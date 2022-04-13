5ef8defba9f16d3a9e3a360c53a3426e56bc3cc9
Novanda Ahsan <nathoilla@indo-soft.com>
2022-04-13 14:12:31 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const windmill = require('@roketid/windmill-react-ui/config')
+const {
+  lightBlue,
+  warmGray,
+  trueGray,
+  coolGray,
+  blueGray,
+  ...colors
+} = defaultTheme.colors
+
+module.exports = windmill({
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './containers/**/*.{js,ts,jsx,tsx}',
+    './example/**/*.{js,ts,jsx,tsx}',
+  ],
+  extend: {
+    colors
+  },
+  plugins: [],
+})
