5bf27127af71fde40398b51763050b8abc433b50
Novanda Ahsan <nathoilla@indo-soft.com>
2022-04-13 16:43:39 +0700

    progress
    @todo delete forgot pass and signup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,19 @@ import React from 'react'
 import { Windmill } from '@roketid/windmill-react-ui'
 import type { AppProps } from 'next/app'
 
+import { UserProvider } from '@supabase/supabase-auth-helpers/react'
+import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
+
 function MyApp({ Component, pageProps }: AppProps) {
   // suppress useLayoutEffect warnings when running outside a browser
-  if (!process.browser) React.useLayoutEffect = React.useEffect;
+  if (!typeof window) React.useLayoutEffect = React.useEffect;
 
   return (
-    <Windmill usePreferences={true}>
-      <Component {...pageProps} />
-    </Windmill>
+       <UserProvider supabaseClient={supabaseClient}>
+          <Windmill usePreferences={true}>
+            <Component {...pageProps} />
+          </Windmill>
+       </UserProvider>
   )
 }
 export default MyApp
