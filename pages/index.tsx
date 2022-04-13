bb5818e53d804e9e7fb2617f674d1e3069a7055f
Novanda Ahsan <novandaahsan1@gmail.com>
2022-04-13 23:13:49 +0700

    better auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,15 @@
-import { supabase } from 'lib/supabase'
-import { useRouter } from 'next/router'
-import React, { useEffect } from 'react'
+import React from 'react'
 import PageTitle from '../components/Typography/PageTitle'
 import Layout from '../containers/Layout'
 
 function Dashboard() {
-  const { replace } = useRouter()
-  const user = supabase.auth.user()
-  // go to login if not authenticated
-  useEffect(() => {
-    if (!user) replace('login')
-  }, [replace, user])
-
-  if (!user) return <></>
-  else
-    return (
-      <Layout>
-        <PageTitle>Dashboard</PageTitle>
+  return (
+    <Layout>
+      <PageTitle>Dashboard</PageTitle>
 
 
-      </Layout>
-    )
+    </Layout>
+  )
 }
 
 export default Dashboard
