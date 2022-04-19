7e35d24f2f7c41d83daae0f5cf7ed89cb87896c9
Novanda Ahsan <novandaahsan1@gmail.com>
2022-04-20 06:35:57 +0700

    init cerate user verif

--- pages/users/create.tsx
+++ pages/users/create.tsx
@@ -1,19 +1,48 @@
 import { Button, HelperText, Input, Label } from "@roketid/windmill-react-ui"
+import { validate } from "class-validator"
 import PageTitle from "components/Typography/PageTitle"
 import Layout from "containers/Layout"
 import Cookies from "cookies"
 import { Formik } from "formik"
+import produce from "immer"
 import { getUserRole, updateToken } from "lib/jwt"
+import { CreateUserDTO } from "lib/types/User"
 import { GetServerSideProps } from "next"
+import { useCallback, useState } from "react"
 
 const CreateUser: React.FC<any> = ({ role }) => {
+  const [createUserInput, setCreateUserInput] = useState<CreateUserDTO>(
+    {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      user_metadata:
+      {
+        fullname: '',
+        nip: 0,
+        unit: ''
+      }
+    }
+  )
+  const onInputChange = useCallback((e) => {
+    setCreateUserInput(
+      produce((draft: any) => {
+        const obj = e.target.name.split('.')
+        if (obj.length === 2)
+          draft[obj[0]][obj[1]] = e.target.value
+        else
+          draft[obj[0]] = e.target.value
+      })
+    )
+  }, [])
+
   return <Layout role={role}>
     <PageTitle>Tambah User</PageTitle>
 
-    <div className="max-w-2xl">
+    <div className="">
 
       <Formik
-        initialValues={{ email: '', password: '' }}
+        initialValues={new CreateUserDTO()}
         validate={values => {
           // if (!values.email) {
           //   errors.email = 'Email wajib diisi';
@@ -30,6 +59,20 @@ const CreateUser: React.FC<any> = ({ role }) => {
           // Object.keys(errors).forEach((key: string) => !errors[key as keyof LoginError] && delete errors[key as keyof LoginError])
 
           // return errors;
+
+          let errors: CreateUserDTO = new CreateUserDTO();
+
+          validate(values).then(ers => {
+            console.log('errors', ers)
+            // errors = ers
+          })
+
+          // console.log('errors', errors)
+
+          errors.password = 'asd'
+
+          return errors
+
         }}
         onSubmit={async (values, { setSubmitting }) => {
           // setSubmitting(true)
@@ -48,40 +91,86 @@ const CreateUser: React.FC<any> = ({ role }) => {
           isSubmitting,
         }) => (
           <form onSubmit={handleSubmit} className="pb-7">
-            <Label>
+            {JSON.stringify(errors)}
+            <Label className='mt-4'>
               <span>Email</span>
               <Input
-                className='mt-1 focus:border-blue-400 focus:ring-blue-300'
-                type="email"
                 name="email"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.email}
-                placeholder='john@doe.com'
-              />
+                value={createUserInput.email}
+                onChange={onInputChange}
+                type="email"
+                className="mt-1"
+                placeholder="sherlyayu@gmail.com" />
             </Label>
             {errors.email && touched.email && (
               <HelperText valid={false}>{errors.email}</HelperText>
             )}
 
-
             <Label className='mt-4'>
               <span>Password</span>
               <Input
-                className='mt-1 focus:border-blue-400 focus:ring-blue-300'
-                type="password"
                 name="password"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.password}
-                placeholder='***************'
-              />
+                value={createUserInput.password}
+                onChange={onInputChange}
+                type="password"
+                className="mt-1"
+                placeholder="rahasia" />
+            </Label>
+            {errors.passwordConfirm && touched.passwordConfirm && (
+              <HelperText valid={false}>{errors.passwordConfirm}</HelperText>
+            )}
+
+            <Label className='mt-4'>
+              <span>Konfirmasi Password</span>
+              <Input type="password" className="mt-1" placeholder="rahasia" />
             </Label>
             {errors.password && touched.password && (
               <HelperText valid={false}>{errors.password}</HelperText>
             )}
-            <Button type='submit' className='mt-10 w-max' disabled={isSubmitting} block>
-              {isSubmitting ? "Memproses..." : "Masuk"}
+
+            <Label className='mt-4'>
+              <span>Nama Lengkap</span>
+              <Input
+                value={createUserInput.user_metadata.fullname}
+                name='user_metadata.fullname'
+                onChange={onInputChange}
+                className="mt-1"
+                placeholder="Sherly Ayu" />
+            </Label>
+            {errors.user_metadata?.fullname && touched.user_metadata?.fullname && (
+              <HelperText valid={false}>{errors.user_metadata?.fullname}</HelperText>
+            )}
+
+            <Label className='mt-4'>
+              <span>NIP</span>
+              <Input
+                value={createUserInput.user_metadata.nip || ''}
+                name="user_metadata.nip"
+                onChange={onInputChange}
+                type="number"
+                className="mt-1"
+                placeholder="123123123123" />
+            </Label>
+            {errors.user_metadata?.nip && touched.user_metadata?.nip && (
+              <HelperText valid={false}>{errors.user_metadata?.nip}</HelperText>
+            )}
+
+            <Label className='mt-4'>
+              <span>Unit</span>
+              <Input
+                name="user_metadata.unit"
+                value={createUserInput.user_metadata.unit}
+                onChange={onInputChange}
+                type="text"
+                className="mt-1"
+                placeholder="Logistik" />
+            </Label>
+            {errors.user_metadata?.unit && touched.user_metadata?.unit && (
+              <HelperText valid={false}>{errors.user_metadata?.unit}</HelperText>
+            )}
+
+            <Button type='submit' className='mt-10 w-max ml-auto' disabled={isSubmitting} block>
+              {isSubmitting ? "Memproses..." : "Tambah"}
             </Button>
             {
               // authError &&
