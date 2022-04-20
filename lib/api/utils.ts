c049542139e118cd6a1780ad969d6428a905f13c
Novanda Ahsan <novandaahsan1@gmail.com>
2022-04-20 11:31:44 +0700

    fix import error

--- lib/api/utils.ts
+++ lib/api/utils.ts
@@ -1,13 +1,17 @@
-import { supabase } from "./supabase"
+import supabase from "./supabase";
 
 export const isNipRegistered = async (nip: number) => {
-  const { data, error } = await supabase.from('users').select('*').eq('nip', nip).single()
-  if (data && !error) return true
-  else return false
-}
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("nip", nip)
+    .single();
+  if (data && !error) return true;
+  else return false;
+};
 
 export const objToUrl = (obj: any) => {
   if (obj) {
-    return 'asd'
+    return "asd";
   }
-}
+};
