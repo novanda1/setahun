d55942b9c76e57cd4b6b920df04f55b563cd0158
Novanda Ahsan <novandaahsan1@gmail.com>
2022-04-21 15:14:58 +0700

    user api: single user update

--- pages/api/users/[id].ts
+++ pages/api/users/[id].ts
@@ -1,14 +1,16 @@
-import { updateUserHandler } from "lib/handlers/userHandlers";
+import {
+  getUserByIdHandler,
+  updateUserHandler,
+} from "lib/handlers/userHandlers";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const id = req.query.id as string;
-
-  if (req.method === "PUT") {
-    updateUserHandler(req, res, id);
-  } else if (req.method === "DELETE") {
-    updateUserHandler(req, res, id);
-  } else {
-    res.end();
+  if (req.method === "GET") getUserByIdHandler(req, res);
+  else if (req.method === "PUT") updateUserHandler(req, res);
+  else {
+    res.statusCode = 400;
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Cache-Control", "max-age=180000");
+    res.end(JSON.stringify({ error: { message: "method not allowed" } }));
   }
 }
