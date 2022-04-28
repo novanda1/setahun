be8f35dc853596c4f954787ee5fe17e39a044798
Novanda Ahsan <novandaahsan1@gmail.com>
2022-04-29 03:26:26 +0700

    better WithAuth middleware

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import styles from "./Loader.module.css";
 
-function Loader() {
+type Props = {
+  text: string;
+};
+
+const Loader = ({ text }: Props) => {
+  if (!text) text = "Please wait...";
   return (
     <div>
       <div className="fixed top-0 left-0 z-50 w-screen h-screen flex items-center justify-center bg-black bg-opacity-30">
         <div className="bg-white border py-2 px-5 rounded-lg flex items-center flex-col">
           <div className={`${styles.LoaderDots} block relative w-20 h-5 mt-2`}>
-            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
-            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
-            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
-            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-green-500"></div>
+            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-blue-500"></div>
+            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-blue-500"></div>
+            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-blue-500"></div>
+            <div className="absolute top-0 mt-1 w-3 h-3 rounded-full bg-blue-500"></div>
           </div>
           <div className="text-gray-500 text-xs font-light mt-2 text-center">
-            Please wait...
+            {text}
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Loader;
--- containers/Layout.tsx
+++ containers/Layout.tsx
@@ -7,6 +7,8 @@ import WithAuth from "lib/withAuth";
 import { useContext } from "react";
 import Main from "./Main";
 import React from "react";
+import { supabase } from "lib/supabase";
+import Loader from "components/Loader/Loader";
 interface ILayout {
   children: React.ReactNode;
   role?: string;
@@ -14,27 +16,35 @@ interface ILayout {
 
 function Layout({ children, role }: ILayout) {
   const { isSidebarOpen } = useContext(SidebarContext);
+  const user = supabase.auth.user();
 
   return (
-    <SidebarProvider>
-      <CreateCertifiedModalProvider>
-        <MeProvider role={role}>
-          <div
-            className={`flex h-screen bg-gray-50 dark:bg-gray-900 ${
-              isSidebarOpen && "overflow-hidden"
-            }`}
-          >
-            <WithAuth>
-              <Sidebar />
-              <div className="flex flex-col flex-1 w-full">
-                <Header />
-                <Main>{children}</Main>
-              </div>
-            </WithAuth>
-          </div>
-        </MeProvider>
-      </CreateCertifiedModalProvider>
-    </SidebarProvider>
+    <>
+      <SidebarProvider>
+        <CreateCertifiedModalProvider>
+          <MeProvider role={role}>
+            <div
+              className={`flex h-screen bg-gray-50 dark:bg-gray-900 ${
+                isSidebarOpen && "overflow-hidden"
+              }`}
+            >
+              <WithAuth>
+                <Sidebar />
+                <div className="flex flex-col flex-1 w-full">
+                  <Header />
+                  <Main>{children}</Main>
+                </div>
+              </WithAuth>
+            </div>
+          </MeProvider>
+        </CreateCertifiedModalProvider>
+      </SidebarProvider>
+      {!user && (
+        <div className="bg-black z-50 absolute inset-0">
+          <Loader text="Waiting for authentication" />
+        </div>
+      )}
+    </>
   );
 }
 
